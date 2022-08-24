7e0f4335d7c8180abc772f2996ec33d2581ba198
riyahmiah <112012073+riyahmiah@users.noreply.github.com>
2022-08-24 13:54:36 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/******************************************************************************************** * Java Program Name 
+*********************************************************************************************
+* Riyah Miah 
+* 8/24/22
+* CMSC255 and 004
+********************************************************************************************/
+
+public class Hello { 
+     public static void main(String[] args) {
+ 	 
+ 	 System.out.println("Riyah Miah");
+ 	 System.out.println("Java Program Name");
+ 	 System.out.println("First Java");
+     System.out.println("8/24/22");
+     System.out.println("CMSC255 004");
+     System.out.println();
+
+    }
+}     
